de308bb4091d7a53e37b236f4ab3eb8d223abb20
vaibhav-deval <vaibhavdeval004@gmail.com>
2025-04-13 16:51:16 +0530

    some bugs fixed and calc is working properly

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-function Solve(val){
-    var v=document.getElementById("out");
-    v.value+=val;
+function Solve(val) {
+  var v = document.getElementById("out");
+  v.value += val;
 }
 
-function result(){
-    var number1=document.getElementById("out").value;
-    try {
-        number1=eval(number1)
-        document.getElementById("out").value=number1;
-    } catch {
-        document.getElementById("out").value="error"
-        
-    }
+function result() {
+  var number1 = document.getElementById("out").value;
+  try {
+    number1 = eval(number1);
+    document.getElementById("out").value = number1;
+  } catch {
+    alert("something went wrong");
+    document.getElementById("out").value = "";
+  }
+  
 
 }
-function Clear(){
-    var v=document.getElementById("out");
-    v.value="";
+function Clear() {
+  var v = document.getElementById("out");
+  v.value = "";
 }
 
-function Back(){
-    var v=document.getElementById("out")
-    v.value=v.value.slice(0,-1);
+function Back() {
+  var v = document.getElementById("out");
+  v.value = v.value.slice(0, -1);
 }
 
-
-document.addEventListener
+document.addEventListener("keydown", function (e) {
+  const key = e.key;
+  const validKeys = "0123456789+-*/.%";
+  if (validKeys.includes(key)) {
+    Solve(key);
+  }
+  else if(key === "Enter"){
+    result();
+  }
+  else if(key==="Backspace"){
+    Back();
+  }
+  else if(key==="C" || key ==="c"){
+    Clear();
+  }
+});
